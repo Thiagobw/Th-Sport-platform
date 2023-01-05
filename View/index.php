11cfa14f0867059163152d3a4127d6af212d67aa
Thiagobw <96150000+Thiagobw@users.noreply.github.com>
2023-01-05 03:00:30 -0300

    init

--- View/index.php
+++ View/index.php
@@ -4,9 +4,44 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!--Import bootstrap-5.1 CSS-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!--Own css-->
+    <link rel="stylesheet" href="CSS/styleHome.css">
     <title>Document</title>
 </head>
 <body>
-    
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container">
+                <a class="navbar-brand" href="#">Logo</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                
+                <div class="collapse navbar-collapse justify-content-between" id="navbarNav">
+
+                    <div></div>
+                    <ul class="navbar-nav">
+                        
+                        <li class="nav-item p-md-1">
+                            <a class="nav-link" href="#">SOBRE</a>
+                        </li>
+
+                        <li class="nav-item p-md-1">
+                            <a class="nav-link" href="#">Log in</a>
+                        </li>
+
+                        <li class="nav-item p-md-1">
+                            <a class="nav-link" id="btn-sign-up" href="#">Sign up</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <!--Import bootstrap-5.1 JS-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 </html>
