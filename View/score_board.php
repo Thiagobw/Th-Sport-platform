4bc648354c31ecdaeee6b509c46c59d78e778a71
Thiagobw <96150000+Thiagobw@users.noreply.github.com>
2023-01-12 18:44:17 -0300

    arquivos principais

--- View/score_board.php
+++ View/score_board.php
@@ -8,7 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <!--Own css-->
     <link rel="stylesheet" href="CSS/styleScoreBoard.css">
-    <title>Document</title>
+    <title>Placar digital</title>
 </head>
 <body>
 
