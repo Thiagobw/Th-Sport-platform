b21dfec061180bcf06c111a2d58f2e1222e110dc
Thiagobw <96150000+Thiagobw@users.noreply.github.com>
2023-01-14 03:02:47 -0300

    estilização da fonte e deixando responsivo

--- View/score_board.php
+++ View/score_board.php
@@ -10,8 +10,30 @@
     <link rel="stylesheet" href="CSS/styleScoreBoard.css">
     <title>Placar digital</title>
 </head>
-<body>
+<body class="container-fluid">
+    <header>
+        <br><br><br><br><br>
+    </header>
 
+    <main class="row">
+        <article class="col-12 col-sm-4 col-md-4 col-lg-4 bg-dark p-2">
+            <h3 class="text-white text-center w-100 h-50 m-0">Atleta</h3>
+            <h1 class="text-white text-center w-100 m-0 h-50">Nome..</h1>
+        </article>
+        
+        <article class="col-12 col-sm-4 col-md-4 col-lg-4 bg-success p-3">
+            <h2 class="text-center" id="timer">05:00</h2>
+        </article>
+        
+        <article class="col-12 col-sm-4 col-md-4 col-lg-4 bg-dark p-2">
+            <h3 class="text-white text-center w-100 h-50 m-0">Atleta</h3>
+            <h1 class="text-white text-center w-100 m-0 h-50">Nome..</h1>
+        </article>
+    </main>
+
+    <footer>
+
+    </footer>
     <!--Import bootstrap-5.1 JS-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
