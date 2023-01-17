c2ec6dfe3f5e6e64b12d3f110dc96d33b2eb459a
Thiagobw <96150000+Thiagobw@users.noreply.github.com>
2023-01-17 04:38:02 -0300

    link de arquivo

--- View/score_board.php
+++ View/score_board.php
@@ -6,8 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!--Import bootstrap-5.1 CSS-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <!--Own css-->
+    <!--Import Own css-->
     <link rel="stylesheet" href="CSS/styleScoreBoard.css">
+    <!--Import Own JS-->
+    <script src="JS/main.js"></script>
     <title>Placar digital</title>
 </head>
 <body class="container-fluid">
